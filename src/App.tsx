04adbc0b9dd702a3390fd29418603b2458f89e83
David Tran Duc <davidtranduc8@gmail.com>
2023-02-11 13:11:37 +0100

    Add a word limit and character deletion

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,18 @@ function App() {
 
   useEffect(() => {
     const keyListener = (event: KeyboardEvent) => {
-      if (event.keyCode >= 65 && event.keyCode <= 90) setWord((word) => word + event.key)
+      if (event.keyCode >= 65 && event.keyCode <= 90 && word.length < 5) {
+        setWord((word) => word + event.key)
+      }
+      if (event.key === 'Backspace') {
+        setWord((word) => word.slice(0, -1))
+      }
     }
 
     addEventListener('keyup', keyListener)
 
     return () => removeEventListener('keyup', keyListener)
-  }, [])
+  }, [word])
 
   return <div>{word}</div>
 }
